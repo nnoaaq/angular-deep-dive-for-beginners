5729bba32bbed10d3afb441a9f303e68f0e4bea1
Noa Julkunen <s1900877@edu.bc.fi>
2021-02-02 12:38:07 +0200

    osio 9 valmis

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,Input } from '@angular/core';
+import { COURSES } from '../../db-data';
+import { Course } from '../model/course';
 @Component({
   selector: 'course-card',
   templateUrl: './course-card.component.html',
   styleUrls: ['./course-card.component.css']
 })
 export class CourseCardComponent implements OnInit {
-
+@Input()
+  course: Course;
   constructor() { }
 
   ngOnInit(): void {
