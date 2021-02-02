95c3a9cd0f637afa375726763f382e44fe37041a
Noa Julkunen <s1900877@edu.bc.fi>
2021-02-02 13:48:28 +0200

    osio 16 valmis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,11 @@ import { Course } from './model/course';
 export class AppComponent {
   courses = COURSES;
  
+  startDate = new Date(2000, 0, 1);
+  price = 9.99;
+  rate = 0.67;
+  course = COURSES[0];
+  title = COURSES[0].description;
   onCourseSelected(course:Course) {
     console.log("App component", course);
 }
